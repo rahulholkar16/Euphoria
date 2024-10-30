278275f89eebd9f740158bc1a3773a58be8c3706
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2024-10-30 14:06:00 +0530

    content and files added

---
+++ college.js
@@ -0,0 +1,14 @@
+const viewMore = document.querySelector(".clg-listing-verified");
+
+viewMore.addEventListener('click',event=>{
+    const current = event.target;
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+    if(!isReadMoreBtn) return;
+    const currentText = event.target.parentNode.querySelector(".read-more-text");
+    currentText.classList.toggle('read-more-text--show');
+    current.textContent = current.textContent.includes("Read More") ? "Read Less..." : "Read More..."
+});
+
+if(viewMore == 'click'){
+    
+}
