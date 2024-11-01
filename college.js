ec39bd6914b13676919ab36b7f073f638aef1c54
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2024-11-01 13:17:03 +0530

    nav feature added

--- college.js
+++ college.js
@@ -1,4 +1,8 @@
 const viewMore = document.querySelector(".clg-listing-verified");
+const hour = document.querySelector(".hours");
+const minute = document.querySelector(".minutes");
+const second = document.querySelector(".seconds");
+const period = document.querySelector(".period");
 
 viewMore.addEventListener('click',event=>{
     const current = event.target;
@@ -9,6 +13,37 @@ viewMore.addEventListener('click',event=>{
     current.textContent = current.textContent.includes("Read More") ? "Read Less..." : "Read More..."
 });
 
-if(viewMore == 'click'){
-    
+
+// ---------------Clock-section----------------
+
+function clock(){
+    var today = new Date();
+
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+
+    let periods = "AM";
+    // set the time period (AM/PM)
+    if(hours >= 12){
+        periods = "PM";
+    }
+
+    //add the 0 for the values lower than 10
+    if(hours < 10){
+        hours = "0" + hours;
+    }
+    if(minutes < 10){
+        minutes = "0" + minutes;
+    }
+    if(seconds < 10){
+        seconds = "0" + seconds;
+    }
+
+    hour.innerHTML = hours;
+    minute.innerHTML = minutes;
+    second.innerHTML = seconds;
+    period.innerHTML = periods;
 }
+
+var updateClock = setInterval(clock, 1000);
